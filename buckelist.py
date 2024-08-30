b5b545c38c7c89cca4163e86f05db9ec8b3dcb4e
uri3lrex <khanhe@tcd.ie>
2024-08-30 17:18:53 +0100

    trying out lists

---
+++ buckelist.py
@@ -0,0 +1,19 @@
+# Write code below 💖
+
+things_to_do = [
+  '🚀 Create the dopest learn to code platform ever.',
+  '⛰️ Go to Japan.',
+  '🏡 Buy a massive fucking house.',
+  '🌏 travel the world.',
+  '🎸 Ace the guitar',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip with the fam and friends.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.'
+]
+
+things_to_do.append(' <3 Write a long fanfic')
+
+for i in things_to_do:
+  print(i)
